5cacda0af9b159756aa977e51a7c75f07bef2ec4
Adem Sahil <adem.sahil274@gmail.com>
2022-10-31 17:54:24 -0700

    js

--- assets/script.js
+++ assets/script.js
@@ -10,22 +10,115 @@ var todayContainer = document.querySelector('#today');
 var forecastContainer = document.querySelector('#forecast');
 var searchHistoryContainer = document.querySelector('#history');
 
-// Fetches weather data for given location from the Weather Geolocation
-// endpoint; then, calls functions to display current and forecast weather data.
-//location argument is an object {
-//	lat: 23423,
-//	lon: arsfe
-//}
-
 // Add timezone plugins to day.js
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-function renderItems(city, data) {
+
+//funstion to display the search history
+function renderSearchHistory(){
+    searchHistoryContainer.innerHTML ='';
+
+    //start at end of history array and count down to show the most recent at the top
+    for (var i= searchHistory.length - 1; i >= 0; i--){
+        var btn = document.createElement('botton');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+
+        //'data-search' allows access to city name when click handler is invoked
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
+    }
+}
+
+    //Function to update history in local storage then updates displayed history
+    function appendToHistory(search) {
+        //if there is no search term then return 'exit the function'
+        if (searchHistory.indexOf(search) !== -1) {
+            return;
+        }
+        searchHistory.push(search);
+
+        localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    }
+
+    //Function to get search history from local Storage
+    function initSearchHistory() {
+        var storedHistory = localStorage.getItem('search-history');
+        if(storedHistory) {
+            searchHistory = JSON.parse(storedHistory);
+        }
+        renderSearchHistory();
+    }
+
+    //Function to display current weather data fetched from OpenWeather API
+    function renderCurrentWeather(city, weather, timezone) {
+        var date = dayjs().tz(timezone).format('M/D/YYYY');
+
+    //store response data from our fetch request in variables
+    var tempF = weather.temp;
+    var windMph = weather.wind-speed;
+    var humidity = weather/humidity;
+    var uvi = weather.uvi;
+    var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`
+    var iconDescription = weather.weather[0].description || weather[0].main;
+
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var heading = document.createElement('h2');
+    var weatherIcon = document.createElement('img');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+    var uvEl = document.createElement('p');
+    var uviBadge = document.createElement('button');
+
+    card.setAttribute('class', 'card');
+    cardBody.setAttribute('class', 'card-body');
+    card.append(cardBody);
+
+    heading.setAttribute('class', 'he card-title');
+    tempEl.setAttribute('class', 'card-text');
+    windEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text')
+
+    heading.textContent = `${city} (${date})`; 
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    weatherIcon.setAttribute('class', 'weather-img');
+    heading.append(weatherIcon);
+    tempEl.textContent = `Temp: ${tempF} F`;
+    windEl.textContent = `Wind: ${windMph} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+
+    uvEl.textContent = 'UV Index: ';
+    uviBadge.classList.add('btn', 'btn-sm');
+
+    if(uvi < 3) {
+        uviBadge,classList.add('btn-success');
+    } else if (uvi < 7) {
+        uviBadge.classList.add('btn-warning');
+    } else {
+        uviBadge.classList.add('btn-danger');
+    }
+
+    uviBadge.textContent = uvi;
+    uvEl.append(uviBadge);
+    cardBody.append(uvEl);
+
+    todayContainer.innerHYML = '';
+    todayContainer.append(card);
+
+ }
+ function renderItems(city, data) {
   // renderCurrentWeather(city, data.current, data.timezone);
   renderForecast(data.daily, data.timezone);
 }
-
+// function fetchWeather(city) {
+//  	var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?q=${city}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
 function fetchWeather(location) {
  	var { lat } = location;
   	var { lon } = location;
@@ -119,14 +212,19 @@ function renderForecastCard(forecast, timezone) {
   forecastContainer.append(col);
 }
 
-fetchWeather({
-	lat: 10,
-	lon: 10,
-	name: "San Diego"
-})
+// fetchWeather({
+// 	lat: 10,
+// 	lon: 10,
+// 	name: "San Diego"
+// })
 
+// function handleSearchFromSumbit(e) {
+//     e.preventDefault();
+//     console.log('hello')
+//     fetchWeather(searchInput.value)
+//     if (!searchInput.value) {
+//         return;
+//     }
+// }
 
-
-
-
-
+searchForm.addEventListener("submit", handleSearchFromSumbit)
